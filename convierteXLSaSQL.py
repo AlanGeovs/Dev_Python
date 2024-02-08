82c2ef1115cf9533c9bc64677708daec52704c56
Alan Geovanni Velázquez Valera <alan@gnesys.com>
2024-02-08 11:31:16 -0600

    Se modificó convierteXLSaSQL.py se agregó la validación de campos vacios, el SQL generado se crea sin errores y se puede comprimir e importar con éxito en PHPMYADMIN

--- convierteXLSaSQL.py
+++ convierteXLSaSQL.py
@@ -1,8 +1,21 @@
 import pandas as pd
 
 # Leer el archivo Excel
-excel_file = 'inventario.xlsx'  # Nombre del archivo actualizado
+excel_file = 'inventario.xlsx'
 df = pd.read_excel(excel_file)
+
+# Función para validar y formatear los valores
+def validate_and_format(value, data_type):
+    if pd.isnull(value):
+        if data_type in ['varchar', 'text']:
+            return ''
+        elif data_type in ['float', 'int']:
+            return 0
+    else:
+        if data_type in ['varchar', 'text']:
+            return str(value).replace("'", "''")  # Manejo de comillas simples para SQL
+        else:
+            return value
 
 # Abrir un archivo para escribir las sentencias SQL
 with open('products.sql', 'w') as sql_file:
@@ -19,8 +32,17 @@ CREATE TABLE products (
     stock INT
 );
 """)
+
     # Para cada fila en el DataFrame, escribir una sentencia INSERT INTO
     for index, row in df.iterrows():
-        sql_file.write(f"INSERT INTO products (SKU, ISBN, description, author, category, price_public, stock) VALUES ('{row['SKU']}', '{row['ISBN']}', '{row['description'].replace("'", "''")}', '{row['author']}', '{row['category']}', {row['price_public']}, {row['stock']});\n")
+        sku = validate_and_format(row['SKU'], 'varchar')
+        isbn = validate_and_format(row['ISBN'], 'varchar')
+        description = validate_and_format(row['description'], 'text')
+        author = validate_and_format(row['author'], 'varchar')
+        category = validate_and_format(row['category'], 'varchar')
+        price_public = validate_and_format(row['price_public'], 'float')
+        stock = validate_and_format(row['stock'], 'int')
 
-print("Archivo SQL generado con éxito.")
+        sql_file.write(f"INSERT INTO products (SKU, ISBN, description, author, category, price_public, stock) VALUES ('{sku}', '{isbn}', '{description}', '{author}', '{category}', {price_public}, {stock});\n")
+
+print("Archivo SQL generado y validado con éxito.")
